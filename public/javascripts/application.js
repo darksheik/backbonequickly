40fc47c370fb2005f20163e24c1f7b3104b5fb56
Don Pflaster <dpflaster@gmail.com>
2012-03-25 12:25:53 -0400

    Finished

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,18 +4,6 @@ App = {
   }
 }
 
-App.SearchRouter = Backbone.Router.extend({
-  routes: {
-    'search/:term' : 'search'
-  },
-  initialize: function() {
-    new App.SearchView({router : this})
-  },
-  search: function(term) {
-    App.SearchController.search(term)
-  }
-})
-
 App.SearchResult = Backbone.Model.extend({})
 App.SearchResultList = Backbone.Collection.extend({
   model: App.SearchResult
@@ -27,6 +15,86 @@ App.SearchController = {
     App.searchResults.add({term : term })
   }
 }
+
+App.SearchResultsView = Backbone.View.extend({
+  el: '#search-results',
+  initialize: function() {
+    this.template = _.template($('#imageTemplate').html())  //Returns a callable function
+    App.searchResults.bind('add',this.renderItem, this)
+  },
+  renderItem: function(model) {
+    $(this.el).show()
+    var view = new App.SearchResultView({model:model})
+    this.$('ul').append(view.el)
+  }
+})
+
+App.SearchResultView = Backbone.View.extend({
+  tagName: 'li',
+  events: {
+    'click a' : 'addFavorite'
+  },
+  initialize: function() {
+    this.template = _.template($('#imageTemplate').html())
+    this.render()
+  },
+  render: function() {
+    var html = this.template({model : this.model.toJSON()})
+    $(this.el).append(html)
+  },
+  addFavorite: function() {
+    App.favorites.add(this.model)
+  }
+})
+App.Favorite = Backbone.Model.extend({})
+App.FavoriteList = Backbone.Collection.extend({
+   model: App.Favorite
+})
+App.favorites = new App.FavoriteList() 
+
+App.FavoritesView = Backbone.View.extend({
+  el: '#favorites',
+  initialize: function() {
+    App.favorites.bind('add', this.renderItem, this)
+    App.favorites.bind('add', this.removeEmptyMessage, this)
+  },
+  renderItem: function(model) {
+    this.$('ul').show()
+    var view = new App.FavoriteView({model:model})
+    this.$('ul').append(view.el)
+    this.$('ul li').show()
+  },
+  removeEmptyMessage: function() {
+    this.$('p').hide()
+  }
+})
+
+App.FavoriteView = Backbone.View.extend({
+  tagName: 'li',
+  initialize: function() {
+    this.template = _.template($('#imageTemplate').html())
+    this.render()
+  },
+  render: function() {
+    var html = this.template({model: this.model.toJSON()})
+    $(this.el).append(html).show()
+  }
+})
+
+App.SearchRouter = Backbone.Router.extend({
+  routes: {
+    'search/:term' : 'search'
+  },
+  initialize: function() {
+    new App.SearchView({router : this})
+    new App.SearchResultsView()
+    new App.FavoritesView()
+  },
+  search: function(term) {
+    App.SearchController.search(term)
+  }
+})
+
 App.SearchView = Backbone.View.extend({
   el: '#search',
   events: {
