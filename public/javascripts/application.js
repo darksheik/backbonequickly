8586b87a1a91fca5112203ab4f984ed100fd1e6f
Don Pflaster <dpflaster@gmail.com>
2012-03-25 05:28:13 -0400

    Added empty model

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,20 +1,44 @@
 App = {
   start: function() {
-    new App.SearchView()
+    new App.SearchRouter()
   }
 }
 
+App.SearchRouter = Backbone.Router.extend({
+  routes: {
+    'search/:term' : 'search'
+  },
+  initialize: function() {
+    new App.SearchView({router : this})
+  },
+  search: function(term) {
+    App.SearchController.search(term)
+  }
+})
+
+App.SearchResult = Backbone.Model.extend({})
+App.SearchResultList = Backbone.Collection.extend({
+  model: App.SearchResult
+})
+App.searchResults = new App.SearchResultList()
+
+App.SearchController = {
+  search: function(term) {
+    App.searchResults.add({term : term })
+  }
+}
 App.SearchView = Backbone.View.extend({
   el: '#search',
   events: {
     'keypress' : 'handleEnter'
   },
   initialize: function() {
+      this.router = this.options.router
       $(this.el).focus()
   },
   handleEnter: function(e) {
     if (e.keyCode == 13) {
-      console.log('enter pressed')
+      this.router.navigate('search/' + $(this.el).val(),true)
     }
   }
 })
